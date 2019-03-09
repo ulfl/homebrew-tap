3182510b902daeaef7430858b948eeafdec6178c
Ulf Leopold <336dev@gmail.com>
2019-03-09 17:44:06 +0100

    Correction.

--- hub.rb
+++ hub.rb
@@ -1,6 +1,6 @@
 class Hub < Formula
   desc "Command line tool for keeping track of common shell commands for yourself and across a development team."
-  url "https://github.com/ulfl/hub/archive/0.3.tar.gz"
+  url "https://github.com/ulfl/hub/archive/0.3.0.tar.gz"
   sha256 "151f4d2c0aa844fe06d9f9c5a80189b91473b115f399a543f41cfc7b704be241"
 
   def install
